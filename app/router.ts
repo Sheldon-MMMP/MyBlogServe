32691ad1ea15d8f6509c195ac5caac447756fe88
Alex <2512989241@qq.com>
2022-10-13 15:18:18 +0800

    添加getDirectory

--- app/router.ts
+++ app/router.ts
@@ -3,5 +3,6 @@ import { Application } from 'egg';
 export default (app: Application) => {
   const { controller, router } = app;
 
-  router.get('/article/:category/:id', controller.article.getArticle);
+  router.get('/article/content/:category/:id', controller.article.getArticle);
+  router.get('/article/directory/:category/:type', controller.article.getDirectory);
 };
